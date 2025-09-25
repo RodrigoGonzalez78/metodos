080812a911707c5235706603e6a9406cfe50b8a4
RodrigoGonzalez78 <gonzlrodrigo@gmail.com>
2025-09-25 13:35:20 -0300

    ejer 2 lab 3

--- lab_3-1/main.py
+++
@@ -1,6 +0,0 @@
-import tanteos.tanteos as tnt
-import 
-
-if __name__ == "__main__":
-    tnt.Metodo_tanteo(lambda x: x**3 - 6*x**2 + 11*x - 6, x_min=0, x_max=4, paso=0.1)
-    tnt.graficar_funcion_con_intervalos(lambda x: x**3 - 6*x**2 + 11*x - 6, [(2.0, 3.0), (3.0, 4.0)], x_min=0, x_max=4)
